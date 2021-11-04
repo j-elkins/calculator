853559b84af339110414066c9bb3bcdf54d97b71
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-11-04 18:56:59 -0400

    Fixed errors, basic functions work correctly

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 // create basic functions for adding, subtracting, multiplying, dividing
-// will need to change console.log to display solution in resultsDiv
+// display solution in resultsDiv
+// round answers with long decimals to fit on the screen
 
 function add(num1, num2) {
   let result = num1 + num2;
-  document.querySelector("#output").value = result;
-  console.log(result);
+  result = Math.round(result * 100000) / 100000;
+  return result;
 }
 
 function subtract(num1, num2) {
   let result = num1 - num2;
-  document.querySelector("#output").value = result;
-  console.log(result);
+  result = Math.round(result * 100000) / 100000;
+  return result;
 }
 
 function multiply(num1, num2) {
   let result = num1 * num2;
-  document.querySelector("#output").value = result;
-  console.log(result);
+  result = Math.round(result * 100000) / 100000;
+  return result;
 }
 
 function divide(num1, num2) {
   let result = num1 / num2;
-  document.querySelector("#output").value = result;
-  console.log(result);
+  result = Math.round(result * 100000) / 100000;
+  return result;
 }
 
 // create function operate() that takes two numbers and an operator
 // operate() calls one of the basic functions and applies to the numbers entered
-
-function operate(num1, num2, operator) {
-  if (operator == "+") {
+function operate(num1, num2, op) {
+  if (op == "+") {
     return add(num1, num2);
-  } else if (operator == "-") {
+  } else if (op == "-") {
     return subtract(num1, num2);
-  } else if (operator == "*") {
+  } else if (op == "*") {
     return multiply(num1, num2);
-  } else if (operator == "/") {
+  } else if (op == "/") {
     return divide(num1, num2);
   }
-  //   console.log(operate);
 }
 
-// console.log(operate(1, 2, "*"));
-
-// need to determine current operator
+// set variables for entered numbers & operator
 let enteredNumber = "";
 let currentOperator = null;
 
-let num1 = 0;
-let num2 = 0;
-let op = null;
+let n1 = 0;
+let n2 = 0;
+
+function doTheMath() {
+  const solution = operate(n1, n2, currentOperator);
+  document.querySelector("#output").value = solution;
+  console.log({ solution });
+  n1 = solution;
+}
 
 // resultsDiv displays numbers and operator as they are entered
 document.querySelector("#btnContainer").addEventListener("click", (event) => {
@@ -63,65 +66,32 @@ document.querySelector("#btnContainer").addEventListener("click", (event) => {
   } else if (target.matches(".btn.op")) {
     let n = parseInt(enteredNumber);
     if (currentOperator == null) {
-      num1 = n;
+      n1 = n;
     } else {
-      num2 = n;
-      op = currentOperator;
+      if (n === 0) {
+        console.log("ERROR"); //console.log works but #output value incorrect..
+        document.querySelector("#output").value = "ERROR";
+      } else {
+        n2 = n;
+      }
+
+      doTheMath();
     }
-    enteredNumber = 0;
+    enteredNumber = "";
 
     currentOperator = target.innerHTML;
-    document.querySelector("#output").value = currentOperator;
+
     console.log(currentOperator);
 
-    // clicking = calls operate() on the input values
-    const equalBtn = document.querySelector("#equalBtn");
-    equalBtn.addEventListener("click", operate(num1, num2, op));
+    // clicking = calls operate() on the entered values
+    // const equalBtn = document.querySelector("#equalBtn");
+    // equalBtn.addEventListener("click", operate(num1, num2, op));
+
+    // clicking another operator also calls operate()
+    // displays solution, and puts solution into num1 to be used in next calculation
   }
-  console.log({ num1, num2, op, enteredNumber });
+  console.log({ n1, n2, currentOperator, enteredNumber });
 });
-
-// // clicking number button stores value in variable num1
-// document.querySelector("#btnContainer").addEventListener("click", (event) => {
-//   let firstClicked = event.target;
-//   if (firstClicked.matches(".btn.num")) {
-//     let num1 = firstClicked.innerHTML;
-//     console.log(num1);
-//   }
-// });
-// // const firstClicked = document.querySelector("btnContainer");
-// // firstClicked.addEventListener("click", save as num1);
-
-// // // clicking operator button stores choice in variable op
-// document.querySelector("#btnContainer").addEventListener("click", (event) => {
-//   let operatorClicked = event.target;
-//   if (operatorClicked.matches(".btn.op")) {
-//     let op = operatorClicked.innerHTML;
-//     console.log(op);
-//   }
-// });
-// // const op = document.querySelector("btn");
-// // op.addEventListener("click", save as op);
-
-// // // clicking next number button stores value in variable num2
-// document.querySelector("#btnContainer").addEventListener("click", (event) => {
-//   let secondClicked = event.target;
-//   if (secondClicked.matches(".btn.num")) {
-//     let num2 = secondClicked.innerHTML;
-//     console.log(num2);
-//   }
-// });
-// const secondClicked = document.querySelector("btn");
-// secondClicked.addEventListener("click", save as num2);
-
-// // update to display solution after = is pressed
-// resultsDiv.textContent = "solution"
-
-// // if num1 + operator + num2 have already been entered- clicking any operator will display currentTotal
-
-// // currentTotal is then used as next num1 of new calculation
-
-// // round answers with long decimals to fit on the screen
 
 // add clear button that clears resultsDiv & all data
 // reload the page? or is there better solution?
@@ -130,8 +100,18 @@ clearBtn.addEventListener("click", () => {
   document.location.reload();
 });
 
-// // ERROR message if user tries to divide by 0
+// // BONUS: add . to let users input decimals. Only allow one . per number (Disable if . is on display)
+// const periodBtn = document.querySelector("#periodBtn");
+// if (#output.value already contains .) {
+//     periodBtn.disabled = true;
+//     periodBtn.classList.add("disabled")
+// }
 
-// // BONUS: add . to let users input decimals. Only allow one . per number
 // // BONUS: add a backspace button to delete the last character entered in display
+let screenView = document.querySelector("#output").value;
+
+const backspaceBtn = document.querySelector("#backspaceBtn");
+backspaceBtn.addEventListener("click", () => {
+  screenView = screenView.value.slice(0, -1);
+});
 // // BONUS: add keyboard support
